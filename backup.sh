89c04c8c1904b2f1ba752d6e4756e9a17b61d947
Thomas Tvedt <thomas@gulost.net>
2018-08-07 22:25:39 +0200

    backup scripot

--- backup.sh
+++ backup.sh
@@ -1,2 +1,5 @@
+echo "Starting backup... !!!"
 s3cmd sync --skip-existing /home/pi/projects/box/Photobooth/assembled s3://photobooth-6770/assembled/
 s3cmd sync --skip-existing /home/pi/projects/box/Photobooth/originals s3://photobooth-6770/originals/
+echo "BACKUP DONE"
+
